41c87197c05b7704c8fa42f6652df43c1ebc768b
Igor Maxsimov <maxsimovwork@gmail.com>
2022-03-14 21:00:40 +0700

    Добавил роутинг.

---
+++ src/Episode.vue
@@ -0,0 +1,15 @@
+<template>
+    Эпизод {{ id }} 
+</template>
+
+<script>
+import Api from './api';
+
+export default {
+    name: 'Episode',
+    props: ['id'],
+}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createWebHashHistory, createRouter } from "vue-router";
+import Main from './Main.vue';
+import Episode from './Episode.vue';
+import EpisodeList from './EpisodeList.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: "/",
+        name: "EpisodeList",
+        component: EpisodeList
+    },
+    {
+      path: "/episode/:id",
+      name: "Episode",
+      component: Episode,
+      props: true
+    },
+  ];
+  
+  const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+  });
+
+createApp(Main).use(router).mount('#app')
